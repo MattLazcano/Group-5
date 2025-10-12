4f05497563076b58581f990d8e8d7da98c30f7c9
MattLazcano <matthew2006lazcano@gmail.com>
2025-10-12 15:10:03 -0400

    started building

--- src/library_name.py
+++ src/library_name.py
@@ -1,1 +1,17 @@
 # Main function library
+
+class LibraryClass:
+    def __init__(self):
+        self.catalog = [] # list of {"id","title","author","genre","copies_total","copies_available"}
+        self.members = {} # dict of member_id: {"name","email","phone"}
+        self.reminders = [] # list of {"member_id","book_id","due_date", "message"}
+        self.loans = [] # list of {"member_id","book_id","loan_date","due_date", "returneed": bool}
+
+
+     # ----------------------------------------------------
+     # SIMPLE function (5-10 lines) BOOK AVAILABILITY CHECK
+     # ----------------------------------------------------
+    def is_book_available(self, book_id):
+        pass
+        
+
